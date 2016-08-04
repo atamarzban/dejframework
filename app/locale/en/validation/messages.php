2a5371e5ca38afb9d840bf764cad84fefce2b3a6
Ata Marzban <atamarzban@gmail.com>
2016-08-04 11:17:30 +0430

    Added Validation (unstable)

---
+++ app/locale/en/validation/messages.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    
+    "required" => "This Field is Required",
+    "string" => "This Field should be an string",
+    "int" => "This Field should be a number",
+    "min" => "This Field should be more than %s",
+    "max" => "This Field should be less than %s",
+    "email" => "This Field should be an email",
+
+];
